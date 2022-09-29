3733a6c4e29b6003436d7224edcdd17a3de33683
Alex Devero <deveroalex@gmail.com>
2022-09-29 10:30:12 +0200

    Add local-storage helper

---
+++ utils/lib/local-storage.ts
@@ -0,0 +1,10 @@
+export const localStorageAvailable = () => {
+  try {
+    const test = 'test';
+    localStorage.setItem(test, test);
+    localStorage.removeItem(test);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
