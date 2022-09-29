55a32405ecf97059cf2be8f133c9ee41014a15ad
Alex Devero <deveroalex@gmail.com>
2022-09-29 11:01:38 +0200

    Remove template literals

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 const protectedPagePath = '/protected';
 
 export const config = {
-  matcher: `${protectedPagePath}/:path*`,
+  matcher: protectedPagePath + '/:path*',
 }
 
 export function middleware(req: NextRequest) {
