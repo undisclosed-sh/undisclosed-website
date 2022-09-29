2f377d123c94ae7ce7cbe9b6f65d34998d6cec87
Alex Devero <deveroalex@gmail.com>
2022-09-29 10:49:31 +0200

    Update homepage text

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Undisclosed
+          Undisclosed
         </h1>
 
         <p className={styles.description}>
