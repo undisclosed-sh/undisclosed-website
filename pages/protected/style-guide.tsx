cd22022b7550aae11d07dd8e198f2fca46d0aaaf
Alex Devero <deveroalex@gmail.com>
2022-10-04 10:41:42 +0200

    Update style guide

--- pages/protected/style-guide.tsx
+++ pages/protected/style-guide.tsx
@@ -1,15 +1,57 @@
+import { useMemo } from 'react'
 import { NextPage } from 'next'
 import styled from 'styled-components'
 
-import { colors } from './../../themes'
+import { PageHead } from '@components';
+import { colors, typographyElements } from '@themes'
 
 const Layout = styled.div`
   padding: 26px;
   background: ${colors.grey[100]};
 `;
 
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const Column = styled.div`
+  width: 50%;
+  padding: 0 26px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  &:first-child {
+    padding-left: 0;
+  }
+
+  &:last-child {
+    padding-right: 0;
+  }
+`;
+
+const PresetWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  width: 100%;
+`;
+
+const PresetElement = styled.div`
+  padding: 8px 0;
+`
+
+const ColorValue = styled.div`
+  padding: 4px 0;
+`
+
 const StyleGuide: NextPage = () => {
-  const colorsList = Object.entries(colors).map(([key, value]) => {
+  const colorsList = useMemo(() => Object.entries(colors).map(([key, value]) => {
     const val = value;
 
     if (typeof val === 'string') {
@@ -22,24 +64,50 @@ const StyleGuide: NextPage = () => {
     } else if (typeof val === 'object') {
       const items = Object.entries(val).map(([key, value]) => {
         return (
-          <div key={key} className="color">
+          <PresetElement key={key} className="color">
             <div className="color__name">{key}</div>
-            <div className="color__value" style={{ background: value }}>{value}</div>
-          </div>
+            <ColorValue className="color__value" style={{ background: value }}>{value}</ColorValue>
+          </PresetElement>
         )
       })
 
       return items
     }
-  })
+  }), [])
+
+  const typography = useMemo(() => Object.entries(typographyElements).map(([key, value]) => {
+    return (
+      <PresetElement key={key} className="color">
+        <div className="color__name" style={{
+          fontSize: value.fontSize,
+          fontWeight: value.fontWeight,
+          lineHeight: value.lineHeight,
+        }}>{key} Lorem ipsum</div>
+      </PresetElement>
+    )
+  }), [])
 
   return (
-    <Layout>
-      <h1>StyleGuide</h1>
+    <>
+      <PageHead pageName="Style guide" />
 
-      <h2>Colors</h2>
-      <div className="colors">{colorsList}</div>
-    </Layout>
+      <Layout>
+
+        <h1>StyleGuide</h1>
+
+        <MainWrapper>
+          <Column>
+            <h2>Typography</h2>
+            <PresetWrapper>{typography}</PresetWrapper>
+          </Column>
+
+          <Column>
+            <h2>Colors</h2>
+            <PresetWrapper className="colors">{colorsList}</PresetWrapper>
+          </Column>
+        </MainWrapper>
+      </Layout>
+    </>
   )
 }
 
