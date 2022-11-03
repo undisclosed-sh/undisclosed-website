4f2c67f0eb56e41833a0d40d92df4160e559bd62
Alex Devero <deveroalex@gmail.com>
2022-11-03 15:30:04 +0100

    Fix broken build

--- pages/protected/style-guide.tsx
+++ pages/protected/style-guide.tsx
@@ -1,17 +1,54 @@
 import { useMemo } from 'react'
 import { NextPage } from 'next'
+import styled from 'styled-components'
 
 import { PageHead } from '@components';
 import { colors, typographyElements } from '@themes'
 
-import {
-  ColorValue,
-  Column,
-  Layout,
-  MainWrapper,
-  PresetElement,
-  PresetWrapper,
-} from './style-guide.styled'
+export const Layout = styled.div`
+  padding: 26px;
+  background: ${colors.grey[100]};
+`;
+
+export const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export const Column = styled.div`
+  width: 50%;
+  padding: 0 26px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  &:first-child {
+    padding-left: 0;
+  }
+
+  &:last-child {
+    padding-right: 0;
+  }
+`;
+
+export const PresetWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  width: 100%;
+`;
+
+export const PresetElement = styled.div`
+  padding: 8px 0;
+`
+
+export const ColorValue = styled.div`
+  padding: 4px 0;
+`
 
 const StyleGuide: NextPage = () => {
   const colorsList = useMemo(() => Object.entries(colors).map(([key, value]) => {
