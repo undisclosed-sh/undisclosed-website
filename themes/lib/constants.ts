5ce2157e4b89d66dd457fb01e8af80ee40c264ea
Alex Devero <deveroalex@gmail.com>
2023-03-02 16:42:18 +0100

    Add constants for breakpoints and spacing

--- themes/lib/constants.ts
+++ themes/lib/constants.ts
@@ -1,1 +1,13 @@
 export const footerHeight = 48;
+
+export const baseSpacing = 16
+
+export const breakpoints = {
+  mobile: '480',
+  tablet: '768',
+  desktop: '1024',
+  large: '1280',
+  xLarge: '1920',
+}
+
+export const containerWidth = 1280
