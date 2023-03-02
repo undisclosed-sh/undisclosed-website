698b7644ef8a38702c213f6aa80f40d4ed1666ac
Alex Devero <deveroalex@gmail.com>
2023-03-02 22:04:40 +0100

    Fix colors

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -10,14 +10,14 @@ import { palette, pxToRem } from '@themes'
 
 const StyledTextarea = styled.textarea`
   width: 100%;
-  border: 1px solid ${palette('grey', 400)};
+  border: 1px solid ${palette('grey', '400')};
   border-radius: ${pxToRem(8)};
   transition: border-color 0.2s ease-out;
 
   &:focus,
   &:hover {
     outline: none;
-    border: 1px solid ${palette('blue', 400)};
+    border: 1px solid ${palette('blue', '400')};
   }
 `
 
@@ -29,12 +29,12 @@ const StyledButton = styled.button`
   border: 0;
   margin-top: ${pxToRem(8)};
   color: ${palette('common', 'white')};
-  background-color: ${palette('blue', 600)};
+  background-color: ${palette('blue', '600')};
   cursor: pointer;
   transition: background-color 0.2s ease-out;
 
   &:hover {
-    background-color: ${palette('blue', 500)};
+    background-color: ${palette('blue', '500')};
   }
 `
 
@@ -48,7 +48,7 @@ const SuccessMessage = styled.span`
   align-self: center;
   border-radius: ${pxToRem(4)};
   color: ${palette('common', 'white')};
-  background-color: ${palette('green', 400)};
+  background-color: ${palette('green', '400')};
 `
 
 const Editor: NextPage = () => {
