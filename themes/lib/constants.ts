33ff246872526abdde2372ddf8b443379a8e6c29
Alex Devero <deveroalex@gmail.com>
2023-03-28 15:17:20 +0200

    Update breakpoints

--- themes/lib/constants.ts
+++ themes/lib/constants.ts
@@ -3,11 +3,10 @@ export const footerHeight = 48;
 export const baseSpacing = 16
 
 export const breakpoints = {
-  mobile: '480',
-  tablet: '768',
-  desktop: '1024',
-  large: '1280',
-  xLarge: '1920',
+  mobile: 0,
+  tablet: 768,
+  desktop: 992,
+  largeDesktop: 1200,
 }
 
 export const containerWidth = 1280
