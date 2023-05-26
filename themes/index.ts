fceb53ff78b7d0109daa4bb67d1386a1439ce5b8
Alex Devero <deveroalex@gmail.com>
2023-05-26 09:41:44 +0200

    Fix build error, add shared styles

--- themes/index.ts
+++ themes/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/colors'
 export * from './lib/constants'
 export * from './lib/palette'
+export * from './lib/shared-styles'
 export * from './lib/spacing'
 export * from './lib/transition'
 export * from './lib/typography'
---
+++ themes/lib/shared-styles/link-styles.ts
@@ -0,0 +1,9 @@
+import { css } from 'styled-components'
+
+export const LinkStyles = css`
+  display: inline-block;
+  width: fit-content;
+  margin-top: 12px;
+  text-decoration: none;
+  border-bottom: 1px solid #adb5bd;
+`
